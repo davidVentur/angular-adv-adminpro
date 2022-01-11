4138224dd35a40be74f201f091fd202b654acac6
David Ventura <davidvillesca70@gmail.com>
2022-01-10 18:55:06 -0600

    seguimos en seccion - 5, video 48 (componente incrementador)

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+     
   ],
   exports: [
     DashboardComponent,
@@ -20,6 +22,6 @@ import { RouterModule } from '@angular/router';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, FormsModule],
 })
 export class PagesModule {}
