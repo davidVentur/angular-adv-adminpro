d635a1009ed905d1ad010b8ad8cb3b603f74349f
David Ventura <davidvillesca70@gmail.com>
2022-06-28 17:31:34 -0500

    video 164

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'
-  ]
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  //Declaracion de formulario
+  public registerForm = this.fb.group({
+    nombre: ['Andy', [Validators.required, Validators.minLength(3)]],
+    email: ['andy@gamil.com', [Validators.required]],
+    password: ['123456', [Validators.required]],
+    confirmPassword: ['123456', [Validators.required]],
+    terms: [false, [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  crearUsuario() {
+    console.log('valor de form', this.registerForm.value);
   }
 
+  ngOnInit(): void {}
 }
